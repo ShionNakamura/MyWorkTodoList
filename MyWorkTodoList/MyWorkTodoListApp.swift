804578db6c3a519ca51776dde90e4df853535d3f
ShionNakamura <shionnakamura0120@gmail.com>
2024-12-04 11:57:41 +0900

    Initial Commit

---
+++ MyWorkTodoList/MyWorkTodoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyWorkTodoListApp.swift
+//  MyWorkTodoList
+//
+//  Created by 仲村士苑 on 2024/12/04.
+//
+
+import SwiftUI
+
+@main
+struct MyWorkTodoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MyWorkTodoListTests/MyWorkTodoListTests.swift
@@ -0,0 +1,17 @@
+//
+//  MyWorkTodoListTests.swift
+//  MyWorkTodoListTests
+//
+//  Created by 仲村士苑 on 2024/12/04.
+//
+
+import Testing
+@testable import MyWorkTodoList
+
+struct MyWorkTodoListTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
